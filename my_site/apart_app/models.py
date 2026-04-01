c5ae4f4b759f74aa69a8323b7f4f6a868d84457a
Aralbekov Umar <umarmirlanbek@gmail.com>
2026-04-01 15:31:52 +0600

    umar

--- my_site/apart_app/models.py
+++ my_site/apart_app/models.py
@@ -1,3 +1,100 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import CharField, ForeignKey
+from phonenumber_field.formfields import PhoneNumberField
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    ROLE_CHOICES = (
+    ('seller','seller'),
+    ('buyer','buyer'),
+    )
+    phone_number = PhoneNumberField()
+    role = models.CharField(choices=ROLE_CHOICES, default='guest', max_length=32)
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=64)
+    country_image = models.ImageField(upload_to='country_photo/')
+
+    def __str__(self):
+        return f'{self.country_name} {self.country_image}'
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=64,unique=True)
+
+    def __str__(self):
+        return self.city_name
+
+class Region(models.Model):
+    region_name = CharField(max_length=128,unique=True)
+    city = ForeignKey(City,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.region_name
+
+class District(models.Model):
+    district_name = models.CharField(max_length=128,unique=True)
+    region = ForeignKey(Region,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.district_name
+
+class Condition(models.Model):
+    condition_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.condition_name
+
+class Property(models.Model):
+    title = models.CharField(max_length=128)
+    PROPERTY_TYPE = (
+    ('apartment','apartment'),
+    ('house','house'),
+    ('commercial property','commercial property'),
+    ('rooms','rooms'),
+    ('parking/garage','parking/garage'),
+    ('country house','country house'),
+    )
+    property_type = models.CharField(unique=True,choices=PROPERTY_TYPE,max_length=32)
+    region = ForeignKey(Region,on_delete=models.CASCADE)
+    city = ForeignKey(City,on_delete=models.CASCADE)
+    country = ForeignKey(Country,on_delete=models.CASCADE)
+    district = models.ForeignKey(District,on_delete=models.CASCADE)
+    area = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
+    price = models.PositiveSmallIntegerField()
+    rooms = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
+                                                       MaxValueValidator(10)],
+                                           null=True,blank=True)
+    floor = models.PositiveSmallIntegerField(validators=[MinValueValidator(1),
+                                                         MaxValueValidator(100)],
+                                             null=True, blank=True)
+    total_floors = models.PositiveSmallIntegerField()
+    condition = models.ForeignKey(Condition,on_delete=models.CASCADE)
+    seller = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+
+    def get_avg_rating(self):
+        rating = self.property_review.all()
+        if rating.exists():
+            return round(sum(i.star for i in rating) / rating.count(),2)
+        return 0
+
+    def get_count_people(self):
+        return self.property_review.count()
+
+class PropertyImage(models.Model):
+    property_image = models.ImageField(upload_to='photo_property/')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+    property = models.ForeignKey(Property,on_delete=models.CASCADE,related_name='property_review')
+    rating  = models.PositiveSmallIntegerField(choices=[(i, str(i))for i in range(1,6)],
+                                            null=True, blank=True)
+    comment = models.TextField(null=True,blank=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.property
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 from pathlib import Path
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -22,6 +23,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -30,17 +32,30 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apart_app',
-    'phonenumber_field',
+    "phonenumber_field",
+    'django_filters',
+    'rest_framework_swagger',
+    'drf_yasg',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'my_site.urls'
@@ -68,10 +83,21 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apart_appteam',
+        'USER': 'postgres',
+        'PASSWORD': 'topor231',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Password validation
@@ -91,21 +117,53 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication'
+                                           ,'rest_framework_simplejwt.authentication.JWTAuthentication')
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = (
+    ('en', 'English'),
+    ('ru', 'Russian'),
+)
+
+MODELTRANSLATION_LANGUAGES = ('ru', 'en')
+
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
+USE_l10N = True
+
 USE_TZ = True
 
+AUTH_USER_MODEL = 'apart_app.UserProfile'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
 
-STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
